0909ca87b5fa6a796138378acd6330a0aaaf2886
Deepansh Sachdeva <deepanshsachdeva@users.noreply.github.com>
2021-12-30 11:39:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const {v4: uuidv4} = require('uuid');
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 app.use(express.static('./build'));
 app.use(cookieParser());
